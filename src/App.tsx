48eefedf8b44ed3e63a451f20e0b06a291dc2400
Dries Verboven <verboven.dries@gmail.com>
2024-02-24 21:33:19 +0100

    adding new text elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Btn } from "./elements";
+import { Btn, Txt } from "./elements";
 
 import "./App.css/";
 
@@ -8,6 +8,19 @@ function App() {
     <main className="main">
       <Btn.Icon icon="IconButton" />
       <Btn.Basic label="Basic Button" onClick={() => fire()} />
+      <Txt.H1>Title</Txt.H1>
+      <Txt.H2>Sub Title</Txt.H2>
+      <Txt.H3>test</Txt.H3>
+      <Txt.Paragraph>
+        "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sun"
+      </Txt.Paragraph>
     </main>
   );
 }
